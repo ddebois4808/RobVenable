9fd07e156defb9812dc17ebfbf5d6a6eb0203484
ddebois4808 <79802564+ddebois4808@users.noreply.github.com>
2024-02-28 22:05:34 -0500

    Testing array

---
+++ common/classDetails.js
@@ -0,0 +1,27 @@
+function StudySkills1Dates() {
+    return [
+        'Monday, July 8 (2:00 - 3:30)',
+        'Monday, July 8 (4:00 - 5:30)',
+        'Tuesday, July 9 (2:00 - 3:30)',
+        'Tuesday, July 9 (4:00 - 5:30)',
+        'Wednesday, July 10 (2:00 - 3:30)',
+        'Wednesday, July 10 (4:00 - 5:30)',
+        'Thursday, July 11 (2:00 - 3:30)',
+        'Thursday, July 11 (4:00 - 5:30)',
+        'Monday, July 15 (2:00 - 3:30)',
+        'Monday, July 15 (4:00 - 5:30)',
+        'Tuesday, July 16 (2:00 - 3:30)',
+        'Tuesday, July 16 (4:00 - 5:30)',
+        'Wednesday, July 17 (2:00 - 3:30)',
+        'Wednesday, July 17 (4:00 - 5:30)',
+        'Thursday, July 18 (2:00 - 3:30)',
+        'Thursday, July 18 (4:00 - 5:30)',
+    ]
+}
+
+
+// const Prices = {
+//     PreparingForMiddleSchool: '$200.00',
+//     Medium: 'medium',
+//     Large: 'large',
+// }
