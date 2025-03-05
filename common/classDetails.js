2a320a7914224ef5dacaea03b8f9813a9d6afed4
ddebois4808 <79802564+ddebois4808@users.noreply.github.com>
2025-03-05 05:53:24 -0500

    Initial 2025 settings

--- common/classDetails.js
+++ common/classDetails.js
@@ -1,32 +1,30 @@
 function StudySkills1Dates() {
     return [
         '-- Select One --',
-        'July 8 - 11, 2:00pm - 3:30pm',
-        'July 8 - 11, 4:00pm - 5:30pm',
-        'July 15 - 18, 2:00pm - 3:30pm',
-        'July 15 - 18, 4:00pm - 5:30pm',
+        'July 14 - 17, 2:00pm - 3:30pm',
+        'July 14 - 17, 4:00pm - 5:30pm',
+        'July 21 - 24, 2:00pm - 3:30pm',
+        'July 21 - 24, 4:00pm - 5:30pm',
     ]
 }
 
 function StudySkills2Dates() {
     return [
-        '-- Select One --',
-        'July 15 - 18, 2:00pm - 3:30pm',
+        '-- Not Available --',
     ]
 }
 
 function HighSchoolDates() {
     return [
-        '-- Select One --',
-        'July 15 - 18, 4:00pm - 5:30pm',
+        '-- Not Available --',
     ]
 }
 
 function ParentWorkshopDates() {
     return [
         '-- Select One --',
-        'Thursday, July 18 (6:00pm - 7:30pm)',
-        'Tuesday, July 23 (6:00pm - 7:30pm)',
+        'Thursday, July 24 (6:00pm - 7:30pm)',
+        'Tuesday, July 29 (6:00pm - 7:30pm)',
         'Not Attending',
     ]
 }
@@ -39,7 +37,7 @@ function StudySkills1Price()
 function StudySkills2Price()
 {
     let today = new Date().getTime();
-    let priceExpire = new Date('06/01/2024').getTime();
+    let priceExpire = new Date('06/01/2025').getTime();
     return today > priceExpire ? '165.00' : '150.00'
 }
 
@@ -50,5 +48,5 @@ function HighSchoolPrice()
 
 function SchoolYear()
 {
-    return '2024'
+    return '2025'
 }
